c29d549170245e384809e1062f4ebc8296913624
Diego Mena <ingelectronicadj@gmail.com>
2017-06-10 15:34:16 -0500

    se añade libreria LiquidCrystal para el manejo de la LCD

--- src/codigo.ino
+++ src/codigo.ino
@@ -3,7 +3,10 @@
  *              LIBRERIAS y CONSTANTES
  ***********************************************************************
  */
-
+ /***** INICIO LCD ******/
+ #include <LiquidCrystal.h>
+ LiquidCrystal lcd(12, 11, 10, 9, 8, 7); // LiquidCrystal(rs, enable, d4, d5, d6, d7)
+ /***** FIN LCD ******/
 
  /*
  ***********************************************************************
